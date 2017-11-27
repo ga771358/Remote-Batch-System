25faf6092e6676bc017d516ccb99dbbc85527edb
yikuan <ga771358@gmail.com>
2017-11-27 21:42:09 +0800

    cgi replace some string for html and add write lock

--- cgi.cpp
+++ cgi.cpp
@@ -20,6 +20,7 @@ typedef struct info {
 	char msg[MAX_BUF];
 	char* pos;
 	int left;
+	bool canwrite;
 } Info;
 
 Info server_info[5];
@@ -27,7 +28,7 @@ Info server_info[5];
 typedef struct data {
 	char ip[20];
 	int port;
-	char filename[20];
+	char filename[100];
 } Data;
 
 Data server_data[5];
@@ -45,36 +46,38 @@ int readline(int fd,char* ptr) {
 
 int main(int argc, char* argv[],char* envp[]) {
 	 
-	char query[MAX_BUF] = "h1=140.113.216.36&p1=1234&f1=t1.txt&h2=140.113.216.36&p2=1235&f2=t2.txt&h3=140.113.216.36&p3=1236&f3=t3.txt&h4=140.113.216.36&p4=1237&f4=t4.txt&h5=140.113.216.36&p5=1238&f5=t5.txt";
-	//query = getenv("QUERY_STRING");
-	cout << query << endl;
+	//char query[MAX_BUF] = "h1=140.113.216.36&p1=1234&f1=t1.txt&h2=140.113.216.36&p2=1235&f2=t2.txt&h3=140.113.216.36&p3=1236&f3=t3.txt&h4=140.113.216.36&p4=1237&f4=t4.txt&h5=140.113.216.36&p5=1238&f5=t5.txt";
+	char parameter[15][100] = {0};
+	char* query = getenv("QUERY_STRING");
+	//cout << query << endl;
 
-	int id = 0;
-	for(char* parameter = strtok(query,"="),*val; parameter != NULL; parameter = strtok(NULL, "=")) {
-	 	if(parameter[0] == 'h') {
-	 		val = strtok(NULL, "&");
-	 		strcpy(server_data[id].ip,val); 
+	int id = 0, i = 1;
+	char* token = strtok(query,"&");
+	strcpy(parameter[0],token);
+	while(token = strtok(NULL, "&")) strcpy(parameter[i++],token);
+
+	for(int k = 0; k < i; k++) {
+		strtok(parameter[k],"=");
+		char* val = strtok(NULL, "&");
+		if(val != NULL) {
+		 	if(parameter[k][0] == 'h') strcpy(server_data[id].ip,val); 
+		 	else if(parameter[k][0] == 'p') server_data[id].port = atoi(val); 
+		 	else if(parameter[k][0] == 'f') sprintf(server_data[id++].filename, "%s" ,val);
 	 	}
-	 	else if(parameter[0] == 'p') {
-	 		val = strtok(NULL, "&");
-	 		server_data[id].port = atoi(val); 
-	 	}
-	 	else if(parameter[0] == 'f') {
-	 		val = strtok(NULL, "&");
-	 		strcpy(server_data[id++].filename,val); 
-	 	}
+	 	else if(parameter[k][0] == 'p') server_data[id++].port = 0;
+	 	
 	}
 
 	int server_num = id, maxfd = 0, total_conn = 0;
-	cout << "Content-type: text/html\r\n\r\n";
-	cout << "<html>\n<head>\n<meta http-equiv=\"Content-Type\" content=\"text/html; charset=big5\" />\n<title>Network Programming Homework 3</title>\n</head>\n";
-	cout << "<body bgcolor=#336699>\n<font face=\"Courier New\" size=2 color=#FFFF99>\n";
-	cout << "<table width=\"800\" border=\"1\">\n";
-	cout << "<tr>\n";
+	cout << "Content-type: text/html\n\n";
+	cout << "<html><head><meta http-equiv=\"Content-Type\" content=\"text/html; charset=big5\" /><title>Network Programming Homework 3</title></head>";
+	cout << "<body bgcolor=#336699><font face=\"Courier New\" size=2 color=#FFFF99>";
+	cout << "<table width=\"800\" border=\"1\">";
+	cout << "<tr>";
 	for(int id = 0; id < server_num; id++) cout << "<td>" << server_data[id].ip << "</td>";
-	cout << "</tr>\n<tr>\n";
+	cout << "</tr><tr>";
 	for(int id = 0; id < server_num; id++) cout << "<td valign=\"top\" id=\"m" << id << "\"></td>";
-	cout << "</tr>\n</table>\n";
+	cout << "</tr></table>";
 			
 	struct sockaddr_in client_sin;
 	fd_set rset, wset, result_rset, result_wset;
@@ -82,6 +85,7 @@ int main(int argc, char* argv[],char* envp[]) {
 	FD_ZERO(&wset);
 
 	for(int id = 0; id < server_num; id++) {
+		if(server_data[id].port == 0) continue;
 	 	Info& server = server_info[id];
  		server.connfd = socket(AF_INET, SOCK_STREAM, 0);
  		client_sin.sin_family = AF_INET;
@@ -89,7 +93,7 @@ int main(int argc, char* argv[],char* envp[]) {
 		client_sin.sin_port = htons(server_data[id].port);
 
 		if(connect(server.connfd, (struct sockaddr *) &client_sin, sizeof(client_sin)) < 0) {
-			perror("connect");
+			//perror("connect");
 			close(server.connfd);
 			server.connfd = -1;
 		}
@@ -99,7 +103,8 @@ int main(int argc, char* argv[],char* envp[]) {
 			FD_SET(server.connfd, &wset);
 			if(server.connfd > maxfd) maxfd = server.connfd;
     		server.batch_file = open(server_data[id].filename , O_RDONLY);
-    		perror("open");
+    		server.canwrite = 0;
+    		//perror("open");
 			server.left = 0;
 		}
 	}
@@ -116,15 +121,19 @@ int main(int argc, char* argv[],char* envp[]) {
 	 			memset(msg, 0, MAX_BUF);
 	 			memset(add_br, 0, MAX_BUF);
 	 			if(read(server.connfd, msg, MAX_BUF) > 0) {
+	 				cout << "<script>document.all['m" << id << "'].innerHTML += \"";
 	 				for(int pos = 0,len = strlen(msg); pos != len; pos++) {
-	 					if(msg[pos] == '\n') strcat(add_br,"<br>");
-	 					else if(msg[pos] == '<') strcat(add_br,"&lt;");
-	 					else if(msg[pos] == '>') strcat(add_br,"&gt;");
-	 					else strncat(add_br,&msg[pos],1);
-	 					
+	 					if(msg[pos] == '\n') cout << "<br>";
+	 					else if(msg[pos] == '<') cout << "&lt;";
+	 					else if(msg[pos] == '>') cout << "&gt;";
+	 					else if(msg[pos] == '"') cout << "&quot;";
+	 					else if(msg[pos] == ' ') cout << "&nbsp;";
+	 					else {
+	 						if(msg[pos] == '%') server.canwrite = 1;
+	 						cout << msg[pos];
+	 					}
 	 				}
-	 				sprintf(response,"<script>document.all['m%d'].innerHTML += \"%s\";</script>",id,add_br);
- 		 			cout << response << endl;
+	 				cout << "\";</script>";
 	 			}
 	 			else {
 	 				close(server.connfd);
@@ -132,7 +141,6 @@ int main(int argc, char* argv[],char* envp[]) {
 	 				FD_CLR(server.connfd,&wset);
 	 				server.connfd = -1;
 	 				total_conn--;
-	 				if(total_conn == 0) cout << "</font>\n</body>\n</html>\n";
 	 			}
 	 		}
 	 		if(FD_ISSET(server.connfd, &result_wset)) {
@@ -142,24 +150,24 @@ int main(int argc, char* argv[],char* envp[]) {
 	 				server.pos = server.msg + nwrite;
 	 				server.left -= nwrite;
 	 			}
-	 			else {
+	 			else if(server.canwrite){
 	 				memset(server.msg, 0 , MAX_BUF);
 	 				memset(add_br, 0 , MAX_BUF);
 	 				int nread = readline(server.batch_file, server.msg);
-	 	
 	 				int nwrite = write(server.connfd, server.msg, nread);
-
+	 				server.canwrite = 0;
 	 				server.pos = server.msg + nwrite;
 	 				server.left = nread - nwrite;
+	 				strtok(server.msg,"\r\n");
+	 				cout << "<script>document.all['m" << id << "'].innerHTML += \"<b>";
 	 				for(int pos = 0,len = strlen(server.msg); pos != len; pos++) {
-	 					if(server.msg[pos] == '>') strcat(add_br,"&gt;");
-	 					else if(server.msg[pos] == '<') strcat(add_br,"&lt;");
-	 					else if(server.msg[pos] == '\n') strcat(add_br, "<br>");
-	 					else if(server.msg[pos] == '\r');
-	 					else strncat(add_br,&server.msg[pos],1);
+	 					if(server.msg[pos] == '>') cout << "&gt;";
+	 					else if(server.msg[pos] == '<') cout << "&lt;";
+	 					else if(server.msg[pos] == '\n') cout << "error!" << endl;
+	 					else if(server.msg[pos] == ' ') cout << "&nbsp;";
+	 					else cout << server.msg[pos];
 	 				}
-	 				sprintf(response,"<script>document.all['m%d'].innerHTML += \"% <b>%s</b><br>\";",id,add_br);
- 		 			cout << response << endl;
+	 				cout << "</b><br>\";</script>";
  		 			if(nread == 0 || strncmp("exit",server.msg,4) == 0) {
 	 					shutdown(server.connfd, SHUT_WR);
 	 					FD_CLR(server.connfd, &wset);
@@ -170,5 +178,6 @@ int main(int argc, char* argv[],char* envp[]) {
 	 	}
 	 }
 
+	 cout << "</font></body></html>";
 	 return 0;
  }
